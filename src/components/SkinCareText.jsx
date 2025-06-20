941c4df61131932e434832f05855dd52314f051d
samrudhkishsanpm <samrudhkishsanpm@gmail.com>
2025-06-20 11:02:37 +0530

    Update SkincareText component styles for improved readability and consistency

--- src/components/SkinCareText.jsx
+++ src/components/SkinCareText.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 
 const SkincareText = () => (
-  <div className="bg-[#FEFFF4] py-12 px-6 text-[53px] leading-[78px] font-[400] tracking-[-0.035em] mx-[100px]">
-    <p className="text-gray-800 ">
+  <div className="bg-[#FEFFF4] mx-[30px] my-50 text-[20px] leading-[30px] tracking-[-0.035em] md:text-[31px] md:leading-[46px] md:tracking-[-0.05em] md:mx-[40px] md:my-[100px] 
+  2xl:text-[53px] 2xl:leading-[78px] font-[400] 2xl:tracking-[-0.035em] 2xl:mx-[100px] 2xl:mt-[100px] 2xl:mb-[300px]">
+    <p className="text-[#2D3B36] ">
       Experience the ultimate in skincare with our expertly formulated products,
       crafted to nourish, protect, and rejuvenate your skin.
-      <span className="font-normal">
+      <span className="">
         {" "}
         Combining the finest natural ingredients with{" "}
       </span>
-      <span className="text-gray-400 ">
+      <span className="text-[#2D3B36] opacity-50 ">
         advanced science, our collection ensures every skin type can achieve a
         radiant, healthy glow. Embrace your beauty with confidence every day.
         Experience the ultimate in skincare with our expertly formulated
